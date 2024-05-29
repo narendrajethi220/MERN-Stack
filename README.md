840c66e5ba075221747dbfa49a3e0b6b2ce043b7
Narendra Jethi <narendrajethi220@gmail.com>
2024-05-29 15:47:24 +0530

    Added Login Functionality

--- README.md
+++ README.md
@@ -17,3 +17,5 @@
 #9 Securing User Password using Bcryptjs
 
 #10 Securing User Authentication with JSON Web Token (JWT)
+
+#11 User Login Route with Functionality
--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user-model");
 
 const home = async (req, res) => {
@@ -21,7 +22,7 @@ const register = async (req, res) => {
 
     const userData = await User.create({ username, email, phone, password });
     res.status(200).json({
-      msg: userData,
+      msg: "User Successfully Registered",
       token: await userData.generateToken(),
       userId: userData._id.toString(),
     });
@@ -30,7 +31,30 @@ const register = async (req, res) => {
     res.status(404).json("Page Not Found !");
   }
 };
-
 // In most cases, converting _id to a string is a good practice bcoz it ensures consistency and compatibility across different JWT libraries and systems. It also aligns with the expectations that claims in a JWT are represented as strings.
 
-module.exports = { home, register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, userExist.password);
+    if (isPasswordValid) {
+      res.status(200).json({
+        msg: "User Login Successfull",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    } else {
+      res.status(401).json({
+        msg: "Invalid Email or Password",
+      });
+    }
+  } catch (error) {
+    res.status(500).json("internal server error");
+  }
+};
+
+module.exports = { home, register, login };
